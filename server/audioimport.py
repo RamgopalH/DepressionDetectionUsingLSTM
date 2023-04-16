56be3befd3accc7193a246234b38131fddd77432
RamgopalH <rg2792002@gmail.com>
2023-04-16 11:08:39 +0530

    connect homepage to main page

--- server/audioimport.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-
-audio_file = "300_AUDIO.wav"
-# import wave
-# samplerate, data = wavfile.read('./300_AUDIO.wav')
-audio_segment = AudioSegment.from_wav(audio_file)
